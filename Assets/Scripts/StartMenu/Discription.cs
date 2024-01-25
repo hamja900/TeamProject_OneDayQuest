9514fc24e7c5d0691d38729a610ff232b8eecd0f
seohyun <leeseohyun61@naver.com>
2024-01-25 10:48:31 +0900

    feat : 게임설명서 기능

---
+++ Assets/Scripts/StartMenu/Discription.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discription : MonoBehaviour
+{
+    public GameObject panel;
+
+   public void OnClickDiscription()
+    {
+        panel.SetActive(true);
+    }
+}
