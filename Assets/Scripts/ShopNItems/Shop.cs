84ce83862ca9f240bf58ff75b02ff729b33fef0a
hamja900 <luru@naver.com>
2024-01-30 12:09:02 +0900

    fix : 솔드아웃 추가

--- Assets/Scripts/ShopNItems/Shop.cs
+++ Assets/Scripts/ShopNItems/Shop.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Android.Types;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +8,8 @@ using UnityEngine.UI;
 public class Shop : MonoBehaviour
 {
     public Text currentPointTxt;
+    public GameObject soldOut1;
+    public GameObject soldOut2;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +20,8 @@ public class Shop : MonoBehaviour
     private void Update()
     {
         ShowCurrentPoint();
+        ShowGoods1();
+        ShowGoods2();
 
     }
 
@@ -27,5 +32,27 @@ public class Shop : MonoBehaviour
         currentPointTxt.text = gameManager.totalPoint.ToString();
     }
 
+    void ShowGoods1()
+    {
+        if (gameManager.bugItem)
+        {
+            soldOut1.SetActive(true);
+        }
+        else
+        {
+            soldOut1.SetActive(false);
+        }
+    }
+    void ShowGoods2()
+    {
+        if(gameManager.speedItem)
+        {
+            soldOut2.SetActive(true);
+        }
+        else
+        {
+            soldOut2.SetActive(false);
+        }
+    }
 
 }
