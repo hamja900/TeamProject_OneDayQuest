1d74bb1d213718cd4eea457efb678565a59504f5
hamja900 <luru@naver.com>
2024-01-30 09:28:13 +0900

    fix : 샵 아이템 추가

--- Assets/Scripts/ShopNItems/Items.cs
+++ Assets/Scripts/ShopNItems/Items.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Items : MonoBehaviour
 {
@@ -9,11 +10,27 @@ public class Items : MonoBehaviour
     public int ExtraBugCount;
     public float ExtraSpeed;
 
+    public Image Icon;
+    public Text NameTxt;
+    public Text DescriptionTxt;
+
+    public ItemSO itemSO;
+
     public Items(string name, string description, int extraBugCount, float extraSpeed)
     {
         this.Name = name;
         this.Description = description;
         this.ExtraBugCount = extraBugCount;
         this.ExtraSpeed = extraSpeed;
+    }
+
+    private void Awake()
+    {
+        Icon = GetComponentsInChildren<Image>()[1];
+        Text[] texts = GetComponentsInChildren<Text>();
+        NameTxt = texts[0];
+        DescriptionTxt = texts[1];
+        NameTxt.text = itemSO.Name;
+        DescriptionTxt.text = itemSO.Description;
     }
 }
